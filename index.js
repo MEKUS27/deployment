356dcf17ab8cae1f2ea13f0e041d5fe077d5fc61
MEKUS27 <vorji325@gmail.com>
2024-07-03 14:08:00 +0100

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import express from "express";
+import axios from "axios";
+import dotenv from "dotenv"
+// const express = require('express');
+// const axios = require('axios');
+// require('dotenv').config();
 
 const app = express();
-const port = 9000;
-app.use("/", (req, res) => {
-    res.json({ message:"Hello From Express App" })
-})
+const PORT = process.env.PORT || 3000;
 
-app.listen(9000, () => {
-    console.log(`Starting server on port ${port}`);
+app.get('/api/hello', async (req, res) => {
+  const visitorName = req.query.visitor_name || 'Guest';
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  
+  const geoResponse = await axios.get(`https://ipapi.co/${clientIp}/json/`);
+  const location = geoResponse.data.city || 'Unknown location';
+
+ 
+  const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`);
+  const temperature = weatherResponse.data.main.temp;
+
+  res.json({
+    client_ip: clientIp,
+    location: location,
+    greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
+  });
+}); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
