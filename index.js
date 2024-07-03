c4eac857422b7bb97d52b4f01880699c3e85dda6
MEKUS27 <vorji325@gmail.com>
2024-07-03 18:23:02 +0100

    first commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const url = require("url");
 const axios = require("axios");
 
 // Function to make a GET request with query parameters using axios
-async function makeApiCall(queryParams) {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=ccfebbb910404ae5bc5101334240107&q=${queryParams}&days=1&aqi=no&alerts=no`;
-  const response = await axios.get(url);
+async function makeApiCall(location) {
+  const apiUrl = `http://api.weatherapi.com/v1/forecast.json?key=ccfebbb910404ae5bc5101334240107&q=${location}&days=1&aqi=no&alerts=no`;
+  const response = await axios.get(apiUrl);
   return response.data;
 }
 
@@ -15,11 +15,10 @@ const server = http.createServer(async (req, res) => {
   if (req.url.startsWith("/api/hello")) {
     try {
       const queryParams = url.parse(req.url, true).query;
-      const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-      const apiResponse = await makeApiCall(ip);
+      const location = queryParams.location || 'London'; // Default to 'London' if no location is provided
+      const apiResponse = await makeApiCall(location);
       res.writeHead(200, { "Content-Type": "application/json" });
       const data = {
-        client_ip: ip, // The IP address of the requester
         location: apiResponse.location.region, // The city of the requester
         greeting: `Hello, ${
           queryParams.visitor_name
@@ -27,7 +26,7 @@ const server = http.createServer(async (req, res) => {
             : "Enter your name with params ?visitor_name=your_name"
         }!, the temperature is ${
           apiResponse.current.temp_c
-        } degrees Celcius in ${apiResponse.location.region}`,
+        } degrees Celsius in ${apiResponse.location.region}`,
       };
 
       res.end(JSON.stringify(data));
