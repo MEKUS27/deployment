991780875374729e1ddb3bb182c79580e67e4d38
MEKUS27 <vorji325@gmail.com>
2024-07-03 14:19:04 +0100

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-import express from "express";
-import axios from "axios";
-import dotenv from "dotenv"
-// const express = require('express');
-// const axios = require('axios');
-// require('dotenv').config();
+// index.js
+const http = require("http");
+const url = require("url");
+const axios = require("axios");
 
-const app = express();
-const PORT = process.env.PORT || 9000;
+// Function to make a GET request with query parameters using axios
+async function makeApiCall(queryParams) {
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=ccfebbb910404ae5bc5101334240107&q=${queryParams}&days=1&aqi=no&alerts=no`;
+  const response = await axios.get(url);
+  return response.data;
+}
 
-app.use('/', async (req, res) => {
-  const visitorName = req.query.visitor_name || 'Guest';
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+// Create a server object
+const server = http.createServer(async (req, res) => {
+  if (req.url.startsWith("/api/hello")) {
+    try {
+      const queryParams = url.parse(req.url, true).query;
+      const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+      const apiResponse = await makeApiCall(ip);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const data = {
+        client_ip: ip, // The IP address of the requester
+        location: apiResponse.location.region, // The city of the requester
+        greeting: `Hello, ${
+          queryParams.visitor_name
+            ? queryParams.visitor_name
+            : "Enter your name with params ?visitor_name=your_name"
+        }!, the temperature is ${
+          apiResponse.current.temp_c
+        } degrees Celcius in ${apiResponse.location.region}`,
+      };
 
-  
-  const geoResponse = await axios.get(`https://ipapi.co/${clientIp}/json/`);
-  const location = geoResponse.data.city || 'Unknown location';
+      res.end(JSON.stringify(data));
+    } catch (error) {
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error calling API");
+    }
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to my simple Node.js app!");
+  }
+});
 
- 
-  const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${process.env.OPENWEATHERMAP_API_KEY}`);
-  const temperature = weatherResponse.data.main.temp;
+// Define the port to listen on
+const port = 3000;
 
-  res.json({
-    client_ip: clientIp,
-    location: location,
-    greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
-  });
-}); 
-
-app.listen(9000, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
